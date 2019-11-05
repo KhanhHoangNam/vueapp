ba7389ac32a77c9efbee020739f8bcb23358bc2b
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2019-11-06 00:47:27 +0700

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    <HelloWorld msg="Đây là các bài học Vue.js"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from './components/HelloWordComponent.vue'
+/**
+ * Một file Vue gồm 3 phần chính: template, script và style
+ */
 export default {
   name: 'app',
   components: {
---
+++ src/components/HelloWordComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWord',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!--"scoped": Chỉ có tác dụng trong file.vue này -->
+<style scoped>
+ h1 {
+   color: red;
+ }
+</style>
