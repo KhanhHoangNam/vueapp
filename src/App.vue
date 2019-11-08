00048bb182ba2943dda5fd55412ed3e7fe98480c
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2019-11-09 00:47:00 +0700

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,23 @@
 <template>
-  <div id="app">    
-    <HelloWorld message="Đây là các bài học Vue.js"/>
-    <FirstComponent message="This is First Component"/>
-    <SecondComponent message="This is Second Component"/>
-    <ThirdComponent message="This is Third Component"/>
+  <div id="container">    
+    <meta name= "viewport" content= "width=device-width, initial-scale=1">
+    <BlogPostsComponent message="Danh sách các BlogPosts"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWordComponent.vue'
-import FirstComponent from './components/FirstComponent'
-import SecondComponent from './components/SecondComponent'
-import ThirdComponent from './components/ThirdComponent'
+import BlogPostsComponent from './components/BlogPostsComponent'
 /**
  * Một file Vue gồm 3 phần chính: template, script và style
  */
 export default {
   name: 'app',
   components: {
-    HelloWorld,
-    FirstComponent,
-    SecondComponent,
-    ThirdComponent
+    BlogPostsComponent,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
